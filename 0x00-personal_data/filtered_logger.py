d4e07923518228ee2b6454d466ce2d4127b6acc2
Tigo-cmd <ja16162040@gmail.com>
2024-11-07 13:08:09 +0100

    Added class RedactingFormatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """this module handles logging for debugging"""
 import re
 from typing import List
+import logging
 
 
 patterns = {
@@ -14,3 +15,24 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """this function returns the log message obfuscated"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+     """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats a LogRecord.
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        text = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return text
